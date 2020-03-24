f1d616265acc1079a836f57953c793603ca36d46
Mohammed Sohail <sohail778899@gmail.com>
2020-03-24 20:05:52 -0300

    Added test cases

--- test_routes.py
+++ test_routes.py
@@ -12,19 +12,6 @@ class BasicTests(unittest.TestCase):
         pass
 
     def test_createUserInvalidEmail(self):
-
-        #encoding input
-        # nameString = "test".encode('ascii')
-        # name = base64.b64encode(nameString)
-        # emailString = "blaabla".encode('ascii')
-        # email = base64.b64encode(emailString)
-        # passwordString = "pass".encode('ascii')
-        # password = base64.b64encode(passwordString)
-
-        # name = base64.b64encode( bytes("test", "utf-8") )
-        # email = base64.b64encode( bytes("blabla", "utf-8") )
-        # password = base64.b64encode( bytes("pass", "utf-8") )
-        #sending the request
         response = self.app.post('/user/create',
         json={'name': 'dGVzdAo=', 'email': 'YmxhYmxhYmxh', 'password': 'aGVsbG8='},
         content_type='application/json',)
@@ -33,10 +20,25 @@ class BasicTests(unittest.TestCase):
         assert data['message'] == 'error'
         assert data['error'] == 'email id given is either not reachable or invalid'
 
+    def test_createUserSuccessTest(self):
+        response = self.app.post('/user/create',
+        json={'name': 'dGVzdAo=', 'email': 'dGVzdGNhc2VzQHRlc3QuY29t', 'password': 'aGVsbG8='},
+        content_type='application/json',)
 
+        data = json.loads(response.get_data(as_text=True))
+        assert data['message'] == 'ok'
 
+    def test_loginFailTest(self):
+        p = (('email','YmxhYmxhYmxh'),('password','aGVsbG8='))
+        response = self.app.get('/user/login',query_string=p)
+        data = json.loads(response.get_data(as_text=True))
+        assert data['message'] == 'error'
 
-
+    def test_loginSuccessTest(self):
+        p = (('email','dGVzdGNhc2VzQHRlc3QuY29t'),('password','aGVsbG8='))
+        response = self.app.get('/user/login',query_string=p)
+        data = json.loads(response.get_data(as_text=True))
+        assert data['message'] == 'ok'
 
 if __name__ == "__main__":
     unittest.main()
