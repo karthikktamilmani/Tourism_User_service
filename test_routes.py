0d298d40d33d994c4f6ce6d79632b052ef9f7539
Karthikk Tamil Mani <kr630601@dal.ca>
2020-05-05 17:30:22 -0300

    Update test_routes.py

--- test_routes.py
+++ test_routes.py
@@ -10,42 +10,35 @@ class BasicTests(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
         pass
-    
-    def test_secret(self):
+
+    def test_createUserInvalidEmail(self):
+        response = self.app.post('/user/create',
+        json={'name': 'dGVzdAo=', 'email': 'YmxhYmxhYmxh', 'password': 'aGVsbG8='},
+        content_type='application/json',)
+
+        data = json.loads(response.get_data(as_text=True))
+        assert data['message'] == 'error'
+        assert data['error'] == 'email id given is either not reachable or invalid'
+
+    def test_createUserSuccessTest(self):
+        response = self.app.post('/user/create',
+        json={'name': 'dGVzdAo=', 'email': 'dGVzdGNhc2VzQHRlc3QuY29t', 'password': 'aGVsbG8='},
+        content_type='application/json',)
+
+        data = json.loads(response.get_data(as_text=True))
+        assert data['message'] == 'ok'
+
+    def test_loginFailTest(self):
+        p = (('email','YmxhYmxhYmxh'),('password','aGVsbG8='))
+        response = self.app.get('/user/login',query_string=p)
+        data = json.loads(response.get_data(as_text=True))
+        assert data['message'] == 'error'
+
+    def test_loginSuccessTest(self):
         p = (('email','dGVzdGNhc2VzQHRlc3QuY29t'),('password','aGVsbG8='))
-        response = self.app.get('/home')
-        #data = json.loads(response.get_data(as_text=True))
-        print(response)
-        assert response == 'HHH'
-
-    # def test_createUserInvalidEmail(self):
-    #     response = self.app.post('/user/create',
-    #     json={'name': 'dGVzdAo=', 'email': 'YmxhYmxhYmxh', 'password': 'aGVsbG8='},
-    #     content_type='application/json',)
-
-    #     data = json.loads(response.get_data(as_text=True))
-    #     assert data['message'] == 'error'
-    #     assert data['error'] == 'email id given is either not reachable or invalid'
-
-    # def test_createUserSuccessTest(self):
-    #     response = self.app.post('/user/create',
-    #     json={'name': 'dGVzdAo=', 'email': 'dGVzdGNhc2VzQHRlc3QuY29t', 'password': 'aGVsbG8='},
-    #     content_type='application/json',)
-
-    #     data = json.loads(response.get_data(as_text=True))
-    #     assert data['message'] == 'ok'
-
-    # def test_loginFailTest(self):
-    #     p = (('email','YmxhYmxhYmxh'),('password','aGVsbG8='))
-    #     response = self.app.get('/user/login',query_string=p)
-    #     data = json.loads(response.get_data(as_text=True))
-    #     assert data['message'] == 'error'
-
-    # def test_loginSuccessTest(self):
-    #     p = (('email','dGVzdGNhc2VzQHRlc3QuY29t'),('password','aGVsbG8='))
-    #     response = self.app.get('/user/login',query_string=p)
-    #     data = json.loads(response.get_data(as_text=True))
-    #     assert data['message'] == 'ok'
+        response = self.app.get('/user/login',query_string=p)
+        data = json.loads(response.get_data(as_text=True))
+        assert data['message'] == 'ok'
         
 
 if __name__ == "__main__":
