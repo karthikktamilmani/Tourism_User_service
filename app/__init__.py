723c824157b88105906b886258f94baef8a70e3e
Karthikk Tamil Mani <kr630601@dal.ca>
2020-05-05 07:53:01 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# app.config.update(
-#     SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/'
-# )
+app.config.update(
+    # SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/'
+    SECRET_KEY=@SECRET_KEY
+)
 
 from app import routes
 
